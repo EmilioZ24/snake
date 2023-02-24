b280fbe18b2dffd2fe4500b399372812c92666f9
Emilio <124ezapata@frhsd.com>
2023-02-24 10:59:05 -0500

    finish scoreboard

--- js/game.js
+++ js/game.js
@@ -6,12 +6,11 @@ let lastRenderTime = 0
 let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
+
 function main(currentTime) {
   if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
-    }
-    return
+    SNAKE_SPEED = 0;
+    
   }
 
 
